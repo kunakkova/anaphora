eea763c1eb34f2dcbe52da94a0060aca8e768044
33sasha81 <sasaderevagina87@gmail.com>
2025-09-28 00:55:25 +0300

    добавление конечных автоматов

--- anaphora/candidates.py
+++ anaphora/candidates.py
@@ -9,38 +9,37 @@ def is_collective_noun(word):
 def is_common_gender_noun(word):
     return normalize_word(word) in common_gender_nouns
 
-def find_candidates(text, pronoun_position):
-    text_lower = text.lower()
-    sentences = get_sentences(text)
-    current_sentence_idx = 0
-    current_pos = 0
-    for i, sentence in enumerate(sentences):
-        sentence_length = len(sentence)
-        if current_pos <= pronoun_position < current_pos + sentence_length:
-            current_sentence_idx = i
-            break
-        current_pos += sentence_length
-    start_sentence_idx = max(0, current_sentence_idx - 3)
-    end_sentence_idx = current_sentence_idx
-    relevant_sentences = sentences[start_sentence_idx:end_sentence_idx+1]
-    if current_sentence_idx > 0:
-        current_sentence_start = sum(len(s) for s in sentences[:current_sentence_idx])
-        relevant_part = text_lower[current_sentence_start:pronoun_position]
-        relevant_sentences[-1] = relevant_part
-    else:
-        relevant_sentences[0] = relevant_sentences[0][:pronoun_position]
-    search_text = " ".join(relevant_sentences)
-    words = get_words(search_text)
-    word_positions = []
-    current_pos = 0
-    for word in words:
-        start = search_text.find(word, current_pos)
-        end = start + len(word)
-        word_positions.append((word, start, end))
-        current_pos = end
-    candidates = []
-    def add_candidate(word, start, end, pos, normalized, gender=None, number=None, is_group=False):
-        candidates.append({
+class ReferentSearchState:
+    START = 'START'
+    LOCATE_SENTENCE = 'LOCATE_SENTENCE'
+    EXTRACT_CONTEXT = 'EXTRACT_CONTEXT'
+    TOKENIZE_CONTEXT = 'TOKENIZE_CONTEXT'
+    COLLECT_NOMINALS = 'COLLECT_NOMINALS'
+    ADD_COORD_GROUPS = 'ADD_COORD_GROUPS'
+    FIND_PRONOUN_WORD = 'FIND_PRONOUN_WORD'
+    ADD_ADDRESSED_ENTITY = 'ADD_ADDRESSED_ENTITY'
+    ADD_SPEAKER_CONTEXT = 'ADD_SPEAKER_CONTEXT'
+    DONE = 'DONE'
+
+
+class ReferentSearchDFA:
+    def __init__(self, text: str, pronoun_position: int):
+        self.text = text
+        self.pronoun_position = pronoun_position
+        self.text_lower = text.lower()
+        self.sentences = get_sentences(text)
+        self.current_sentence_idx = 0
+        self.current_sentence_start = 0
+        self.start_sentence_idx = 0
+        self.end_sentence_idx = 0
+        self.search_text = ''
+        self.word_positions = []
+        self.pronoun_word = None
+        self.candidates = []
+        self.state = ReferentSearchState.START
+
+    def add_candidate(self, word, start, end, pos, normalized, gender=None, number=None, is_group=False):
+        self.candidates.append({
             'word': word,
             'start': start,
             'end': end,
@@ -50,64 +49,140 @@ def find_candidates(text, pronoun_position):
             'number': number,
             'is_group': is_group
         })
-    for word, start, end in word_positions:
-        parsed = morph.parse(word)[0]
-        pos = parsed.tag.POS
-        normalized_word = normalize_word(word)
-        gender = parsed.tag.gender
-        number = parsed.tag.number
-        if pos == 'NOUN' or normalized_word in all_pronouns:
-            vy_forms = {'вы', 'вас', 'вам', 'вами'}
-            if normalized_word in vy_forms:
-                add_candidate(word, start, end, pos, normalized_word, None, 'sing')
-                add_candidate(word, start, end, pos, normalized_word, None, 'plur')
-            elif is_common_gender_noun(word):
-                add_candidate(word, start, end, pos, normalized_word, 'masc', number)
-                add_candidate(word, start, end, pos, normalized_word, 'femn', number)
-            elif is_collective_noun(word):
-                add_candidate(word, start, end, pos, normalized_word, gender, 'sing')
-                add_candidate(word, start, end, pos, normalized_word, gender, 'plur')
-            else:
-                add_candidate(word, start, end, pos, normalized_word, gender, number)
-    full_search_text = " ".join(sentences[start_sentence_idx:end_sentence_idx+1])
-    coord_groups = find_coord_groups(full_search_text)
-    for group in coord_groups:
-        if len(group) > 1:
-            group_text = " и ".join(group)
-            add_candidate(group_text, -1, -1, 'NOUN', group_text.lower(), None, 'plur', True)
-    pronoun_word = None
-    for word, start, end in word_positions:
-        absolute_start = current_sentence_start + start if current_sentence_idx > 0 else start
-        if absolute_start <= pronoun_position <= absolute_start + len(word):
-            pronoun_word = word
-            break
-    if pronoun_word:
-        for i in range(max(0, current_sentence_idx - 1), current_sentence_idx + 1):
-            if i < len(sentences):
-                sentence_to_check = sentences[i]
-                addressed_entity = find_addressed_entity(pronoun_word, sentence_to_check)
-                if addressed_entity:
-                    words_in_address = addressed_entity.split()
-                    main_noun = None
-                    for w in words_in_address:
-                        if get_pos(w) == 'NOUN':
-                            main_noun = w
-                            break
-                    if main_noun:
-                        parsed = morph.parse(main_noun)[0]
-                        gender = parsed.tag.gender
-                        number = parsed.tag.number
-                    else:
-                        gender = None
-                        number = 'plur'
-                    add_candidate(addressed_entity, -1, -1, 'NOUN', addressed_entity.lower(), gender, number)
+
+    def step(self) -> bool:
+        if self.state == ReferentSearchState.START:
+            self.state = ReferentSearchState.LOCATE_SENTENCE
+            return True
+
+        if self.state == ReferentSearchState.LOCATE_SENTENCE:
+            current_pos = 0
+            for i, sentence in enumerate(self.sentences):
+                sentence_length = len(sentence)
+                if current_pos <= self.pronoun_position < current_pos + sentence_length:
+                    self.current_sentence_idx = i
+                    self.current_sentence_start = current_pos
                     break
-    if pronoun_word:
-        speaker = get_speaker_context(pronoun_word, text)
-        if speaker:
-            parsed = morph.parse(speaker)[0]
-            gender = parsed.tag.gender
-            number = parsed.tag.number
-            add_candidate(speaker, -1, -1, 'NOUN', speaker.lower(), gender, number)
-    return candidates
+                current_pos += sentence_length
+            self.start_sentence_idx = max(0, self.current_sentence_idx - 3)
+            self.end_sentence_idx = self.current_sentence_idx
+            self.state = ReferentSearchState.EXTRACT_CONTEXT
+            return True
+
+        if self.state == ReferentSearchState.EXTRACT_CONTEXT:
+            relevant_sentences = self.sentences[self.start_sentence_idx:self.end_sentence_idx+1]
+            if self.current_sentence_idx > 0:
+                relevant_part = self.text_lower[self.current_sentence_start:self.pronoun_position]
+                relevant_sentences[-1] = relevant_part
+            else:
+                relevant_sentences[0] = relevant_sentences[0][:self.pronoun_position]
+            self.search_text = " ".join(relevant_sentences)
+            self.state = ReferentSearchState.TOKENIZE_CONTEXT
+            return True
+
+        if self.state == ReferentSearchState.TOKENIZE_CONTEXT:
+            words = get_words(self.search_text)
+            current_pos = 0
+            self.word_positions = []
+            for word in words:
+                start = self.search_text.find(word, current_pos)
+                end = start + len(word)
+                self.word_positions.append((word, start, end))
+                current_pos = end
+            self.state = ReferentSearchState.COLLECT_NOMINALS
+            return True
+
+        if self.state == ReferentSearchState.COLLECT_NOMINALS:
+            for word, start, end in self.word_positions:
+                parsed = morph.parse(word)[0]
+                pos = parsed.tag.POS
+                normalized_word = normalize_word(word)
+                gender = parsed.tag.gender
+                number = parsed.tag.number
+                if pos == 'NOUN' or normalized_word in all_pronouns:
+                    vy_forms = {'вы', 'вас', 'вам', 'вами'}
+                    if normalized_word in vy_forms:
+                        self.add_candidate(word, start, end, pos, normalized_word, None, 'sing')
+                        self.add_candidate(word, start, end, pos, normalized_word, None, 'plur')
+                    elif is_common_gender_noun(word):
+                        self.add_candidate(word, start, end, pos, normalized_word, 'masc', number)
+                        self.add_candidate(word, start, end, pos, normalized_word, 'femn', number)
+                    elif is_collective_noun(word):
+                        self.add_candidate(word, start, end, pos, normalized_word, gender, 'sing')
+                        self.add_candidate(word, start, end, pos, normalized_word, gender, 'plur')
+                    else:
+                        self.add_candidate(word, start, end, pos, normalized_word, gender, number)
+            self.state = ReferentSearchState.ADD_COORD_GROUPS
+            return True
+
+        if self.state == ReferentSearchState.ADD_COORD_GROUPS:
+            full_search_text = " ".join(self.sentences[self.start_sentence_idx:self.end_sentence_idx+1])
+            coord_groups = find_coord_groups(full_search_text)
+            for group in coord_groups:
+                if len(group) > 1:
+                    group_text = " и ".join(group)
+                    self.add_candidate(group_text, -1, -1, 'NOUN', group_text.lower(), None, 'plur', True)
+            self.state = ReferentSearchState.FIND_PRONOUN_WORD
+            return True
+
+        if self.state == ReferentSearchState.FIND_PRONOUN_WORD:
+            self.pronoun_word = None
+            for word, start, end in self.word_positions:
+                absolute_start = self.current_sentence_start + start if self.current_sentence_idx > 0 else start
+                if absolute_start <= self.pronoun_position <= absolute_start + len(word):
+                    self.pronoun_word = word
+                    break
+            self.state = ReferentSearchState.ADD_ADDRESSED_ENTITY
+            return True
+
+        if self.state == ReferentSearchState.ADD_ADDRESSED_ENTITY:
+            if self.pronoun_word:
+                for i in range(max(0, self.current_sentence_idx - 1), self.current_sentence_idx + 1):
+                    if i < len(self.sentences):
+                        sentence_to_check = self.sentences[i]
+                        addressed_entity = find_addressed_entity(self.pronoun_word, sentence_to_check)
+                        if addressed_entity:
+                            words_in_address = addressed_entity.split()
+                            main_noun = None
+                            for w in words_in_address:
+                                if get_pos(w) == 'NOUN':
+                                    main_noun = w
+                                    break
+                            if main_noun:
+                                parsed = morph.parse(main_noun)[0]
+                                gender = parsed.tag.gender
+                                number = parsed.tag.number
+                            else:
+                                gender = None
+                                number = 'plur'
+                            self.add_candidate(addressed_entity, -1, -1, 'NOUN', addressed_entity.lower(), gender, number)
+                            break
+            self.state = ReferentSearchState.ADD_SPEAKER_CONTEXT
+            return True
+
+        if self.state == ReferentSearchState.ADD_SPEAKER_CONTEXT:
+            if self.pronoun_word:
+                speaker = get_speaker_context(self.pronoun_word, self.text)
+                if speaker:
+                    parsed = morph.parse(speaker)[0]
+                    gender = parsed.tag.gender
+                    number = parsed.tag.number
+                    self.add_candidate(speaker, -1, -1, 'NOUN', speaker.lower(), gender, number)
+            self.state = ReferentSearchState.DONE
+            return True
+
+        if self.state == ReferentSearchState.DONE:
+            return False
+
+        return False
+
+    def run(self):
+        while self.step():
+            pass
+        return self.candidates
+
+
+def find_candidates(text, pronoun_position):
+    dfa = ReferentSearchDFA(text, pronoun_position)
+    return dfa.run()
 
--- anaphora/reflexive.py
+++ anaphora/reflexive.py
@@ -2,6 +2,194 @@ from typing import Iterable, Optional, Union, List, Dict
 import re
 from .helpers import is_subject_simple
 
+class ReflexiveFilterState:
+    START = 'START'
+    PRECHECK_IDIOMS = 'PRECHECK_IDIOMS'
+    PREPARE = 'PREPARE'
+    SPLIT_LEFT_RIGHT = 'SPLIT_LEFT_RIGHT'
+    SELECT_LEFT = 'SELECT_LEFT'
+    FALLBACK_RIGHT = 'FALLBACK_RIGHT'
+    DONE = 'DONE'
+
+
+class ReflexiveFilterDFA:
+    def __init__(self, candidates: Iterable[Dict], pronoun: str, morph, sentence_text: str, idioms: Optional[Iterable[str]] = None):
+        self.candidates = list(candidates or [])
+        self.pronoun = pronoun
+        self.morph = morph
+        self.sentence_text = sentence_text or ""
+        self.idioms = list(idioms or [])
+        self.state = ReflexiveFilterState.START
+        self.sent_low = self.sentence_text.lower()
+        self.pron_low = (self.pronoun or '').lower().strip()
+        self.pron_start = -1
+        self.left_candidates: List[Dict] = []
+        self.right_candidates: List[Dict] = []
+        self.result: Optional[Union[Dict, List[Dict]]] = None
+
+    def _is_plural_token(self, candidate: Dict) -> bool:
+        if not candidate:
+            return False
+        num = candidate.get('number')
+        if num:
+            num_s = str(num).lower()
+            if any(x in num_s for x in ("plur", "pl", "plural", "мн")):
+                return True
+        if candidate.get("is_group"):
+            return True
+        if candidate.get("word", "").lower() in {"они", "мы", "вы"}:
+            return True
+        return False
+
+    def _collect_subject_group_by_text(self, subject: Dict) -> List[Dict]:
+        if subject is None:
+            return []
+        group = [subject]
+        subj_start = subject.get("start", 0)
+        subj_end = subject.get("end", subj_start)
+        for c in self.candidates:
+            if c is subject:
+                continue
+            cstart = c.get("start", 0)
+            cend = c.get("end", cstart)
+            left, right = sorted((subj_end, cstart))
+            between = self.sentence_text[left:right].lower()
+            if re.search(r'\bи\b', between) or re.search(r',\s*и\b', between):
+                pos = (c.get("pos") or "").upper()
+                if pos in {"NOUN", "PROPN", "PRON"}:
+                    group.append(c)
+        group = sorted({id(x): x for x in group}.values(),
+                       key=lambda x: x.get("start", 0))
+        return group
+
+    def _sort_by_closest_left(self, candidates_list):
+        return sorted(candidates_list,
+                      key=lambda c: self.pron_start - c.get("end", c.get("start", 0)),
+                      reverse=True)
+
+    def _sort_by_closest_right(self, candidates_list):
+        return sorted(candidates_list,
+                      key=lambda c: c.get("start", 0) - self.pron_start)
+
+    def step(self) -> bool:
+        if self.state == ReflexiveFilterState.START:
+            self.state = ReflexiveFilterState.PRECHECK_IDIOMS
+            return True
+
+        if self.state == ReflexiveFilterState.PRECHECK_IDIOMS:
+            for idi in self.idioms:
+                if idi and idi.lower() in self.sent_low:
+                    self.result = None
+                    self.state = ReflexiveFilterState.DONE
+                    return True
+            self.state = ReflexiveFilterState.PREPARE
+            return True
+
+        if self.state == ReflexiveFilterState.PREPARE:
+            self.pron_start = self.sent_low.find(self.pron_low)
+            if self.pron_start == -1:
+                self.result = None
+                self.state = ReflexiveFilterState.DONE
+                return True
+            self.left_candidates = [c for c in self.candidates if c.get("start", float("inf")) < self.pron_start]
+            self.right_candidates = [c for c in self.candidates if c.get("start", 0) > self.pron_start]
+            self.state = ReflexiveFilterState.SPLIT_LEFT_RIGHT
+            return True
+
+        if self.state == ReflexiveFilterState.SPLIT_LEFT_RIGHT:
+            plural_subject_groups = []
+            single_subjects = []
+            plural_groups = []
+            single_words = []
+            for c in self.left_candidates:
+                word = c.get("word", "")
+                is_subject = word and is_subject_simple(word, self.sentence_text, self.morph)
+                is_plural = self._is_plural_token(c)
+                if is_plural and is_subject:
+                    plural_subject_groups.append(c)
+                elif is_subject:
+                    single_subjects.append(c)
+                elif is_plural:
+                    plural_groups.append(c)
+                else:
+                    single_words.append(c)
+            if plural_subject_groups:
+                subject = self._sort_by_closest_left(plural_subject_groups)[0]
+                group = self._collect_subject_group_by_text(subject)
+                self.result = group if group else subject
+                self.state = ReflexiveFilterState.DONE
+                return True
+            if single_subjects:
+                subject = self._sort_by_closest_left(single_subjects)[0]
+                self.result = subject
+                self.state = ReflexiveFilterState.DONE
+                return True
+            if plural_groups:
+                subject = self._sort_by_closest_left(plural_groups)[0]
+                group = self._collect_subject_group_by_text(subject)
+                self.result = group if group else subject
+                self.state = ReflexiveFilterState.DONE
+                return True
+            if single_words:
+                nouns_pronouns = [c for c in single_words
+                                  if c.get("pos", "").upper() in {"NOUN", "PROPN", "PRON"}]
+                if nouns_pronouns:
+                    self.result = self._sort_by_closest_left(nouns_pronouns)[0]
+                else:
+                    self.result = self._sort_by_closest_left(single_words)[0]
+                self.state = ReflexiveFilterState.DONE
+                return True
+            self.state = ReflexiveFilterState.FALLBACK_RIGHT
+            return True
+
+        if self.state == ReflexiveFilterState.FALLBACK_RIGHT:
+            if self.right_candidates:
+                plural_subject_groups_right = []
+                single_subjects_right = []
+                plural_groups_right = []
+                single_words_right = []
+                for c in self.right_candidates:
+                    word = c.get("word", "")
+                    is_subject = word and is_subject_simple(word, self.sentence_text, self.morph)
+                    is_plural = self._is_plural_token(c)
+                    if is_plural and is_subject:
+                        plural_subject_groups_right.append(c)
+                    elif is_subject:
+                        single_subjects_right.append(c)
+                    elif is_plural:
+                        plural_groups_right.append(c)
+                    else:
+                        single_words_right.append(c)
+                if plural_subject_groups_right:
+                    subject = self._sort_by_closest_right(plural_subject_groups_right)[0]
+                    group = self._collect_subject_group_by_text(subject)
+                    self.result = group if group else subject
+                elif single_subjects_right:
+                    self.result = self._sort_by_closest_right(single_subjects_right)[0]
+                elif plural_groups_right:
+                    subject = self._sort_by_closest_right(plural_groups_right)[0]
+                    group = self._collect_subject_group_by_text(subject)
+                    self.result = group if group else subject
+                elif single_words_right:
+                    nouns_pronouns = [c for c in single_words_right
+                                      if c.get("pos", "").upper() in {"NOUN", "PROPN", "PRON"}]
+                    if nouns_pronouns:
+                        self.result = self._sort_by_closest_right(nouns_pronouns)[0]
+                    else:
+                        self.result = self._sort_by_closest_right(single_words_right)[0]
+            self.state = ReflexiveFilterState.DONE
+            return True
+
+        if self.state == ReflexiveFilterState.DONE:
+            return False
+
+        return False
+
+    def run(self) -> Optional[Union[Dict, List[Dict]]]:
+        while self.step():
+            pass
+        return self.result
+
 def _is_plural_token(candidate: Dict) -> bool:
     if not candidate:
         return False
@@ -45,94 +233,6 @@ def filter_reflexive_candidates(candidates: Iterable[Dict],
                                 sentence_text: str,
                                 idioms: Optional[Iterable[str]] = None
                                 ) -> Optional[Union[Dict, List[Dict]]]:
-    idioms = list(idioms or [])
-    sent_low = (sentence_text or "").lower()
-    pron_low = (pronoun or "").lower().strip()
-    for idi in idioms:
-        if idi and idi.lower() in sent_low:
-            return None
-    candidates = [
-        c for c in (candidates or [])
-        if c.get("word", "").lower() not in {"себя", "собой", "самого себя"}
-    ]
-    pron_pos = sent_low.find(pron_low)
-    if pron_pos == -1:
-        return None
-    pron_start = pron_pos
-    left_candidates = [c for c in candidates if c.get("start", float("inf")) < pron_start]
-    plural_subject_groups = []
-    single_subjects = []
-    plural_groups = []
-    single_words = []
-    for c in left_candidates:
-        word = c.get("word", "")
-        is_subject = word and is_subject_simple(word, sentence_text, morph)
-        is_plural = _is_plural_token(c)
-        if is_plural and is_subject:
-            plural_subject_groups.append(c)
-        elif is_subject:
-            single_subjects.append(c)
-        elif is_plural:
-            plural_groups.append(c)
-        else:
-            single_words.append(c)
-    def sort_by_closest(candidates_list):
-        return sorted(candidates_list,
-                     key=lambda c: pron_start - c.get("end", c.get("start", 0)),
-                     reverse=True)
-    if plural_subject_groups:
-        subject = sort_by_closest(plural_subject_groups)[0]
-        group = _collect_subject_group_by_text(subject, candidates, sentence_text)
-        return group if group else subject
-    if single_subjects:
-        subject = sort_by_closest(single_subjects)[0]
-        return subject
-    if plural_groups:
-        subject = sort_by_closest(plural_groups)[0]
-        group = _collect_subject_group_by_text(subject, candidates, sentence_text)
-        return group if group else subject
-    if single_words:
-        nouns_pronouns = [c for c in single_words
-                         if c.get("pos", "").upper() in {"NOUN", "PROPN", "PRON"}]
-        if nouns_pronouns:
-            return sort_by_closest(nouns_pronouns)[0]
-        return sort_by_closest(single_words)[0]
-    right_candidates = [c for c in candidates if c.get("start", 0) > pron_start]
-    if right_candidates:
-        plural_subject_groups_right = []
-        single_subjects_right = []
-        plural_groups_right = []
-        single_words_right = []
-        for c in right_candidates:
-            word = c.get("word", "")
-            is_subject = word and is_subject_simple(word, sentence_text, morph)
-            is_plural = _is_plural_token(c)
-            if is_plural and is_subject:
-                plural_subject_groups_right.append(c)
-            elif is_subject:
-                single_subjects_right.append(c)
-            elif is_plural:
-                plural_groups_right.append(c)
-            else:
-                single_words_right.append(c)
-        def sort_right_by_closest(candidates_list):
-            return sorted(candidates_list,
-                         key=lambda c: c.get("start", 0) - pron_start)
-        if plural_subject_groups_right:
-            subject = sort_right_by_closest(plural_subject_groups_right)[0]
-            group = _collect_subject_group_by_text(subject, candidates, sentence_text)
-            return group if group else subject
-        if single_subjects_right:
-            return sort_right_by_closest(single_subjects_right)[0]
-        if plural_groups_right:
-            subject = sort_right_by_closest(plural_groups_right)[0]
-            group = _collect_subject_group_by_text(subject, candidates, sentence_text)
-            return group if group else subject
-        if single_words_right:
-            nouns_pronouns = [c for c in single_words_right
-                             if c.get("pos", "").upper() in {"NOUN", "PROPN", "PRON"}]
-            if nouns_pronouns:
-                return sort_right_by_closest(nouns_pronouns)[0]
-            return sort_right_by_closest(single_words_right)[0]
-    return None
+    dfa = ReflexiveFilterDFA(candidates, pronoun, morph, sentence_text, idioms)
+    return dfa.run()
 
